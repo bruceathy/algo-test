6a3cf300824c71feea596f9c1efd4d0ba51047ea
bruceathy <67832593+bruceathy@users.noreply.github.com>
2024-12-16 22:37:13 -0500

    init

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,11 @@
+function fizzBuzz(num) {
+  if (num % 3 === 0) {
+    return "fizz";
+  } else if (num % 5 === 0) {
+    return "buzz";
+  } else {
+    return num;
+  }
+}
+
+module.exports = fizzBuzz;
